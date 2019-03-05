eae56b8a8387e9db0ededf7c93bb52e9a7376aea
TakkuMattsu <3356758+TakkuMattsu@users.noreply.github.com>
2019-03-05 12:41:22 +0900

    Revert "リスト表示"
    This reverts commit f4f34cae0cc734957dc2fcf64900fcea2b5eb140.

--- app/src/main/java/com/example/takkumattsu/rxweather/MainActivity.kt
+++ app/src/main/java/com/example/takkumattsu/rxweather/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.support.annotation.RawRes
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import android.widget.TextView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -18,45 +16,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val listView = findViewById<ListView>(R.id.list)
-        val adapter = PrefecturesAdapter(this)
-        listView.adapter = adapter
+        val text = findViewById<TextView>(R.id.hello_text)
         readPrefecture(R.raw.prefectures)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .flatMap { prefecture: Prefecture ->
-                    Weather.loadWeather(prefecture)
-                }
-                .subscribe({ prefectureWeather: PrefectureWeather ->
-                    adapter.addOrReplace(prefectureWeather)
-                }, { e ->
-                    // TODO: 本当はUIに反映させたい
-                    Log.d("RxWeather", "${e.localizedMessage}")
-                })
+                .subscribe { Log.d("RxWeather", "prefecture ${it.name}") }
     }
 
     private fun readPrefecture(@RawRes from: Int): Observable<Prefecture> {
-        return Observable
+        // jsonを読み込む
+        val observablePrefecture: Observable<List<Prefecture>> = Observable
                 .just(from) // Observable Int に変換
-                .map { id: Int ->
-                    // raw の json を読み込んで Observable<String> に変換
-                    resources.openRawResource(id)
+                .map {      // raw の json を読み込んで Observable<String> に変換
+                    resources.openRawResource(it)
                             .bufferedReader()
-                            .use {
-                                it.readText()
-                            }
+                            .use { it.readText() }
                 }
-                .map { json: String ->
-                    // String を Gson で Array<Array<String> にして、 Observable<List<Prefecture> に変換
-                    val objects = Gson().fromJson<Array<Array<String>>>(json, object : TypeToken<Array<Array<String>>>() {}.type)
-                    objects.map { p: Array<String> ->
-                        Prefecture(p[0], p[1], p[2])
+                .map {      // String を Gson で Array<Array<String> にして、 Observable<List<Prefecture> に変換
+                    val objects = Gson().fromJson<Array<Array<String>>>(it, object : TypeToken<Array<Array<String>>>() {}.type)
+                    objects.map {
+                        Prefecture(it[0], it[1], it[2])
                     }
-                }.flatMap { list: List<Prefecture> ->
+                }
+        return observablePrefecture
+                .flatMap { list ->
                     // Observable<List<Prefecture> を List<Observable<Preficture>>
-                    val obs: List<Observable<Prefecture>> = list.map { p: Prefecture ->
-                        Observable.just(p)
-                    }
+                    val obs: List<Observable<Prefecture>> = list.map { Observable.just(it) }
                     // List<Observable<Preficture>> を 順番通りに Observable<Preficture> に変換
                     Observable.concat(obs)
                 }
