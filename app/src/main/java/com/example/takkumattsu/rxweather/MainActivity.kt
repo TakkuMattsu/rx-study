9f47e249ff7faf257604b562681ffc00ad78245e
TakkuMattsu <3356758+TakkuMattsu@users.noreply.github.com>
2019-03-05 12:41:35 +0900

    Revert "json読み込み"
    This reverts commit c0efe30b0dbdddf689405341282414251e24aa78.

--- app/src/main/java/com/example/takkumattsu/rxweather/MainActivity.kt
+++ app/src/main/java/com/example/takkumattsu/rxweather/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.takkumattsu.rxweather
 
-import android.os.Bundle
-import android.support.annotation.RawRes
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,33 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val text = findViewById<TextView>(R.id.hello_text)
-        readPrefecture(R.raw.prefectures)
-                .subscribeOn(Schedulers.io())
+        val timer = Observable.interval(1, TimeUnit.SECONDS, Schedulers.newThread()).take(4)
+        val counter = Observable.just(3L, 2L, 1L, 0L )
+                Observable
+                .zip(timer, counter, BiFunction<Long, Long, Long> {_ , num -> num })
+                .doOnEach { Log.d("RxWeather", "do on each $it") }
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { Log.d("RxWeather", "prefecture ${it.name}") }
-    }
-
-    private fun readPrefecture(@RawRes from: Int): Observable<Prefecture> {
-        // jsonを読み込む
-        val observablePrefecture: Observable<List<Prefecture>> = Observable
-                .just(from) // Observable Int に変換
-                .map {      // raw の json を読み込んで Observable<String> に変換
-                    resources.openRawResource(it)
-                            .bufferedReader()
-                            .use { it.readText() }
-                }
-                .map {      // String を Gson で Array<Array<String> にして、 Observable<List<Prefecture> に変換
-                    val objects = Gson().fromJson<Array<Array<String>>>(it, object : TypeToken<Array<Array<String>>>() {}.type)
-                    objects.map {
-                        Prefecture(it[0], it[1], it[2])
-                    }
-                }
-        return observablePrefecture
-                .flatMap { list ->
-                    // Observable<List<Prefecture> を List<Observable<Preficture>>
-                    val obs: List<Observable<Prefecture>> = list.map { Observable.just(it) }
-                    // List<Observable<Preficture>> を 順番通りに Observable<Preficture> に変換
-                    Observable.concat(obs)
-                }
+                .subscribe(
+                        { text.text = "$it" },
+                        {},
+                        { text.text= "Hello RX" })
     }
 }
